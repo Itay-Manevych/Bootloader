bd18d9bf99d8816b6c44f332dc8e78f4d0c21f32
ItayFrisk <itay.manevych@gmail.com>
2026-02-18 20:06:42 +0200

    Add paranetheses to define

--- drivers/vga/vga.h
+++ drivers/vga/vga.h
@@ -4,7 +4,7 @@
 #define VGA_BUFFER_ADDRESS 0xB8000
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
-#define VGA_SCREEN_SIZE VGA_WIDTH * VGA_HEIGHT
+#define VGA_SCREEN_SIZE (VGA_WIDTH * VGA_HEIGHT)
 #define GET_FLAT_INDEX(row, col) ((row) * VGA_WIDTH + col)
 enum { 
     VGA_COLOR_DEFAULT = 0x07, // light gray
@@ -16,3 +16,4 @@ void vga_sync_cursor_from_hw();
 void vga_flush_cursor();
 void vga_set_color(byte color);
 void vga_putc(char c);
+void vga_print_string(const char* string);
