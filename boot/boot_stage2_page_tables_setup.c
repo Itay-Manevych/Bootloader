a61fa0ff186bf8acccdd54cf9c20cf13e83a8a55
ItayFrisk <itay.manevych@gmail.com>
2026-02-18 20:04:54 +0200

    Update page table base address to qword

--- boot/boot_stage2_page_tables_setup.c
+++ boot/boot_stage2_page_tables_setup.c
@@ -71,7 +71,7 @@ void fill_identity_pt(PageTable* pt, qword base)
     }
 }
 
-dword pml4_table_physical = 0;
+qword pml4_table_physical = 0;
 
 void setup_page_tables() 
 {
@@ -89,5 +89,5 @@ void setup_page_tables()
 
     initliaze_pdt_table(pdt);
 
-    pml4_table_physical = (dword)(uintptr_t)pml4;
+    pml4_table_physical = (qword)(uintptr_t)pml4;
 }
